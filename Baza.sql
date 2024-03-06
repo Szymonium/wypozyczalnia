e17b3e0359c7c32577521fe6f966b5369760b015
BoredSzymon <szymonkarpiszen@gmail.com>
2024-03-06 20:25:52 +0100

    update database creation file

--- Baza.sql
+++ Baza.sql
@@ -1,57 +1,161 @@
-CREATE TABLE samochody (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nr_rejestracyjny VARCHAR(8) NULL,
-  marka VARCHAR(20) NULL,
-  model VARCHAR(20) NULL,
-  rocznik VARCHAR(4) NULL,
-  cena_za_dzien FLOAT NULL,
-  PRIMARY KEY(id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 06, 2024 at 08:24 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-CREATE TABLE pracownicy (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  imie VARCHAR(20) NULL,
-  nazwisko VARCHAR(30) NULL,
-  pesel VARCHAR(11) NULL,
-  plec VARCHAR(1) NULL,
-  data_ur DATE NULL,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE klienci (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  imie VARCHAR(20) NULL,
-  nazwisko VARCHAR(30) NULL,
-  pesel VARCHAR(11) NULL,
-  nr_telefonu VARCHAR(9) NULL,
-  data_ur DATE NULL,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE wypozyczenia (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  pracownicy_id INTEGER UNSIGNED NOT NULL,
-  klienci_id INTEGER UNSIGNED NOT NULL,
-  samochody_id INTEGER UNSIGNED NOT NULL,
-  data_wypozyczenia DATE NULL,
-  data_zwrotu DATE NULL,
-  kwota FLOAT NULL,
-  PRIMARY KEY(id),
-  INDEX wypozyczenia_FKIndex1(pracownicy_id),
-  INDEX wypozyczenia_FKIndex2(klienci_id),
-  INDEX wypozyczenia_FKIndex3(samochody_id),
-  FOREIGN KEY(pracownicy_id)
-    REFERENCES pracownicy(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(klienci_id)
-    REFERENCES klienci(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(samochody_id)
-    REFERENCES samochody(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rentakar`
+--
+CREATE DATABASE IF NOT EXISTS `rentakar` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `rentakar`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `imie` varchar(20) DEFAULT NULL,
+  `nazwisko` varchar(30) DEFAULT NULL,
+  `pesel` varchar(11) DEFAULT NULL,
+  `nr_telefonu` varchar(9) DEFAULT NULL,
+  `data_ur` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pracownicy`
+--
+
+CREATE TABLE `pracownicy` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `imie` varchar(20) DEFAULT NULL,
+  `nazwisko` varchar(30) DEFAULT NULL,
+  `pesel` varchar(11) DEFAULT NULL,
+  `plec` varchar(1) DEFAULT NULL,
+  `data_ur` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `samochody`
+--
+
+CREATE TABLE `samochody` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nr_rejestracyjny` varchar(8) DEFAULT NULL,
+  `marka` varchar(20) DEFAULT NULL,
+  `model` varchar(20) DEFAULT NULL,
+  `rocznik` varchar(4) DEFAULT NULL,
+  `cena_za_dzien` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wypozyczenia`
+--
+
+CREATE TABLE `wypozyczenia` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `pracownicy_id` int(10) UNSIGNED NOT NULL,
+  `klienci_id` int(10) UNSIGNED NOT NULL,
+  `samochody_id` int(10) UNSIGNED NOT NULL,
+  `data_wypozyczenia` date DEFAULT NULL,
+  `data_zwrotu` date DEFAULT NULL,
+  `kwota` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `samochody`
+--
+ALTER TABLE `samochody`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `wypozyczenia_FKIndex1` (`pracownicy_id`),
+  ADD KEY `wypozyczenia_FKIndex2` (`klienci_id`),
+  ADD KEY `wypozyczenia_FKIndex3` (`samochody_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `samochody`
+--
+ALTER TABLE `samochody`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `wypozyczenia`
+--
+ALTER TABLE `wypozyczenia`
+  ADD CONSTRAINT `wypozyczenia_ibfk_1` FOREIGN KEY (`pracownicy_id`) REFERENCES `pracownicy` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `wypozyczenia_ibfk_2` FOREIGN KEY (`klienci_id`) REFERENCES `klienci` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `wypozyczenia_ibfk_3` FOREIGN KEY (`samochody_id`) REFERENCES `samochody` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
